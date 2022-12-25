0457244f50027beee821ec6360c58b09296dcee0
Mistyttm <emmey.leo@gmail.com>
2022-12-25 23:02:51 +1000

    attempting to get code generation working

--- events/modalSubmit.js
+++ events/modalSubmit.js
@@ -1,4 +1,38 @@
+require('dotenv').config();
+
+const fs = require('fs');
 const { Events } = require('discord.js');
+
+const emailToken = process.env.SECURITYTOKEN;
+const email = process.env.EMAIL;
+
+function sendEmail(recipient, code){
+    Email.send({
+        SecureToken : emailToken,
+        To : recipient,
+        From : email + "@qut.edu.au",
+        Subject : "Discord Verification Code",
+        Body : "Here is your verification code for the <i>IT Crew</i> Discord Server:\n\n<h1>" + code + "</h1>\n\nPlease enter this code into the Discord server to verify your account.\n\n<b>If you did not request this code, please ignore this email.</b>"
+    })//.then(
+    //     message => alert(message)
+    // );
+}
+
+function generateCode(user) {
+    let code = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 4; i++)
+        code += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+
+    let codeString = user+"-"+code;
+
+    const stream = fs.createWriteStream('../active-codes.txt', { flags: 'a' });
+    stream.write(codeString + ",");
+
+    return code;
+}
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -17,6 +51,7 @@ module.exports = {
 
                 if (staffIdRegex.test(userId) || studentIdRegex.test(userId)) {
                     console.log(userId);
+                    generateCode(userId);
                     await interaction.reply('ID successfully submitted.');
                 } else {
                     await interaction.reply('Invalid ID entered, please try again.');
@@ -25,7 +60,7 @@ module.exports = {
         } catch (error) {
             console.error(error);
             // Issues here with followUp vs reply creating timeout issues
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     }
 };
