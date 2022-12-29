8741be3d43cfc48ad885c5d8f75a637e83b098d8
Mistyttm <emmey.leo@gmail.com>
2022-12-29 20:36:38 +1000

    purge command

---
+++ commands/purge.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Deletes Multiple messages at once')
+        .addIntegerOption((option) =>
+            option.setName('amount')
+                .setDescription('amount of messages to delete')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    async execute(interaction) {
+        try {
+            const amount = interaction.options.getInteger('amount');
+            await interaction.channel.bulkDelete(amount, true);
+            await interaction.reply({ content: `Deleted ${amount} messages`, ephemeral: true });
+        } catch (err) {
+            console.log(err);
+            interaction.reply({ content: 'An error occurred, sorry!', ephemeral: true });
+        }
+    }
+}
