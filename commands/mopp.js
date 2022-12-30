407434f8e991dcaa9de299a31f7c9823bda76cd2
Mistyttm <emmey.leo@gmail.com>
2022-12-30 14:59:00 +1000

    all major commands have been ported

---
+++ commands/mopp.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const pjson = require('../package.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mopp')
+        .setDescription('MOPP info'),
+    async execute(interaction) {
+        try {
+
+            const infoEmbed = new EmbedBuilder()
+                .setColor(0x00008B)
+                .setTitle(`${pjson.name} v${pjson.version}: MOPP | Student Code of conduct`)
+                .setDescription('This command is being used to remind you about the QUT MOPP.\n\nIt is the student code of conduct and provides useful information on things like collusion.\n\nIf you are seeing this you should probably take a look at this page\n\n\n\n**http://www.mopp.qut.edu.au/E/E_02_01.jsp**')
+                .setAuthor({ name: 'CutieBot', iconURL: 'https://cdn.discordapp.com/avatars/1055472048899641365/4d6ff8bb2f760373dd8a41e77300e73a.webp?size=32' })
+                .setURL('http://www.mopp.qut.edu.au/E/E_02_01.jsp');
+                await interaction.reply({
+                    embeds: [infoEmbed],
+                });
+        } catch (err) {
+            console.log(err);
+            interaction.reply({ content: 'An error occurred, sorry!', ephemeral: true });
+        }
+    }
+}
--- commands/weather.js
+++ commands/weather.js
@@ -10,6 +10,15 @@ weather.setLang('en');
 weather.setCoordinate(27.4785, 153.0284);
 weather.setUnits('metric');
 weather.setAPPID(token);
+let temperature;
+weather.getTemperature(function(err, temp){
+    temperature = temp;
+});
+let weathDesc;
+weather.getDescription(function(err, desc){
+    weathDesc = desc;
+});
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -21,7 +30,7 @@ module.exports = {
             const infoEmbed = new EmbedBuilder()
                 .setColor(0x00008B)
                 .setTitle(`${pjson.name} v${pjson.version}: Weather at QUT`)
-                .setDescription(`the current temperature is ${weather.getTemperature()}°C\n\n${weather.getDescription()}`)
+                .setDescription(`the current temperature is ${temperature}°C\n\n${weathDesc}`)
                 .setAuthor({ name: 'CutieBot', iconURL: 'https://cdn.discordapp.com/avatars/1055472048899641365/4d6ff8bb2f760373dd8a41e77300e73a.webp?size=32' });
                 await interaction.reply({
                     embeds: [infoEmbed],
