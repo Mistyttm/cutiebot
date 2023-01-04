Mistyttm/cutiebot

commit 12424b3e640eb5b4e167ea2062784467869eb9bf

move helpers/emailBody.js to helpers/emails/verificationBody.js, fix minor formatting issues

deploy.js
@@ -12,7 +12,8 @@ const commandFiles = readdirSync('./commands').filter((file) => file.endsWith('.
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-    const command = await import(`./commands/${file}`)
+    const command = import(`./commands/${file}`);
+
     commands.push(command.default.data.toJSON());
 }
 

commands/mopp.js
@@ -6,7 +6,7 @@ It is the student code of conduct and provides useful information on things like
 
 If you are seeing this you should probably take a look at this page:
                 
-**http://www.mopp.qut.edu.au/E/E_02_01.jsp**`
+**http://www.mopp.qut.edu.au/E/E_02_01.jsp**`;
 
 export default {
     data: new SlashCommandBuilder()

events/modalSubmit.js
@@ -1,9 +1,5 @@
 import { Events } from 'discord.js';
-<<<<<<< HEAD
 import { emailBody } from '../helpers/emails/verificationEmail.js';
-=======
-import { emailBody } from '../helpers/emailBody.js';
->>>>>>> c51a845 (update imports for modalSubmit.js, update exports for email.js and emailBody.js)
 import sendEmail from '../helpers/email.js';
 import { findUser, saveVerificationCode } from '../helpers/data.js';
 

helpers/emailBody.js
@@ -1,105 +1,0 @@
-const emailBody = (guildName, userName, code) => {
-    return `<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap"
-            rel="stylesheet"
-        />
-    </head>
-    <body>
-        <style>
-            .ExternalClass p,
-            .ExternalClass span,
-            .ExternalClass font,
-            .ExternalClass td {
-                line-height: 100%;
-            }
-            .ExternalClass {
-                width: 100%;
-            }
-
-            @media screen and (-webkit-min-device-pixel-ratio:0) {
-                .main-container {
-                    margin: auto;
-                    padding: 20px 32px;
-                    text-align: left;
-                    min-width: min-content;
-                    font-size: 14px;
-                    font-family: "Open Sans", sans-serif;
-                    color: #212121;
-                    background-color: #FFFFFF;
-                }
-
-                .heading-text {
-                    font-weight: bold;
-                    font-size: 40px;
-                    padding: 32px 3.2px;
-                }
-
-                .main-text {
-                    padding: 3.2px 3.2px 8px;
-                    line-height: 1.75;
-                }
-
-                .verification-code {
-                    padding: 32px 0px 40px 0px;
-                    font-family: JetBrains Mono, sans-serif;
-                    font-size: 80px;
-                    color: #232323;
-                    font-weight: bold;
-                    letter-spacing: 5px;
-                    text-align: center;
-                }
-
-                .footnote {
-                    padding: 6.4px 3.2px;
-                    font-size: 12px;
-                }
-
-                #username {
-                    font-weight: bold;
-                }
-
-                @media (width >= 640px) {
-                    .main-container {
-                        max-width: 720px;
-                        padding-top: 24px;
-                    }
-
-                    .heading-text {
-                        font-size: 48px;
-                        padding-bottom: 36px;
-                    }
-                }
-            }
-        </style>
-        <div class="main-container">
-            <div class="heading-text">
-                Your verification code for ${guildName}
-            </div>
-            <div class="main-text">
-                <div class="greeting-text">
-                    Hi <span id="username">${userName}</span>,
-                    <br />
-                    Your verification code is:
-                </div>
-                <div class="verification-code">${code}</div>
-                <div class="footer-text">Thank you for using our bot.</div>
-            </div>
-            <hr />
-            <div class="footnote">
-                You are receiving this message because this student number was
-                used to verify a Discord account. If you did not request this
-                then please disregard this message.
-            </div>
-        </div>
-    </body>
-</html>`;
-};
-
-export { emailBody };

commit f0a4d6587cb6fd07cde42381b4ed736f70de56ff

fix issue introduced by https://github.com/Mistyttm/cutiebot/commit/12424b3e640eb5b4e167ea2062784467869eb9bf

deploy.js
@@ -12,7 +12,7 @@ const commandFiles = readdirSync('./commands').filter((file) => file.endsWith('.
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-    const command = import(`./commands/${file}`);
+    const command = await import(`./commands/${file}`);
 
     commands.push(command.default.data.toJSON());
 }