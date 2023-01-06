5b06480ad24f642189fb2455de953f25df8f9a9b
Lillian King <lillianfk@proton.me>
2023-01-06 13:40:40 +1100

    update changelog & remove redundant eslint override

--- helpers/emails/verificationEmail.js
+++ helpers/emails/verificationEmail.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-irregular-whitespace */
-/* eslint-disable max-len */
 const emailBody = (guildName, userName, code) => `
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
