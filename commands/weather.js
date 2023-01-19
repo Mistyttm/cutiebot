87282ee2aced50642ef7df156e51becdfe1b9e45
Lillian King <lillianfk@proton.me>
2023-01-19 21:04:08 +1000

    refactor: improve readability + naming, and note changes to API

--- commands/weather.js
+++ commands/weather.js
@@ -13,32 +13,6 @@ const weatherEmbed = new EmbedBuilder()
         iconURL: 'https://cdn.discordapp.com/avatars/1055472048899641365/4d6ff8bb2f760373dd8a41e77300e73a.webp?size=32',
     });
 
-// Fetch data from the API
-const getWeatherData = async (location) => {
-    const url = buildUrl(WEATHER_API_KEY, location);
-    const res = await fetch(url);
-
-    if (!res.ok) {
-        throw new Error('WEATHER: Sorry, an error occurred whilst fetching data.');
-    }
-
-    const data = await res.json();
-
-    // Check for error response from API (e.g. when invalid data is supplied)
-    if (data.error) {
-        throw new Error(`WEATHER: Sorry, ${data.error?.message.toLowerCase()}`);
-    }
-
-    // Format the data and return it as an object
-    const modifiedData = {
-        resolvedLocation: `${data.location?.name}, ${localiseAustralianResults(data.location)}`,
-        temperature: Math.round(data.current?.temp_c),
-        description: data.current?.condition?.text,
-        image: `https:${data.current?.condition?.icon}`,
-    };
-    return modifiedData;
-};
-
 /**
  * Creates url to get the current weather information from https://weatherapi.com/.
  * @param {String} apiKey API key for weatherapi.com
@@ -48,13 +22,37 @@ const getWeatherData = async (location) => {
  * `default` co-ordinates for QUT
  * @returns API url
  */
-const buildUrl = (apiKey, location='-27.4785213,153.0261705') =>
-    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
+const buildUrl = (apiKey, location) =>
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location ?? '-27.4785213,153.0261705'}`;
+
+// Fetch data from the API
+const getWeatherData = async (locationOption) => {
+    const res = await fetch(buildUrl(WEATHER_API_KEY, locationOption));
+
+    if (!res.ok) {
+        throw new Error('WEATHER: Sorry, an error occurred whilst fetching data.');
+    }
+    const data = await res.json();
+
+    // Check for error response from API (e.g. when invalid data is supplied)
+    // Currently doesn't do anything because API now responds with 400 instead of 200 for invalid data
+    if (data.error) {
+        throw new Error(`WEATHER: Sorry, ${data.error?.message.toLowerCase()}`);
+    }
+    // Format the data and return it as an object
+    const weatherData = {
+        location: `${data.location?.name}, ${localiseResults(data.location)}`,
+        temperature: Math.round(data.current?.temp_c),
+        description: data.current?.condition?.text,
+        image: `https:${data.current?.condition?.icon}`,
+    };
+    return weatherData;
+};
 
 // Check if the location for the data from the API is in Australia so that the result can be modified to include the
 // state instead of the country — helps with differentiating duplicate place names.
 // e.g. without this, 'Manly, QLD' and 'Manly, NSW' would both show up as 'Manly, Australia'
-const localiseAustralianResults = (locationData) => {
+const localiseResults = (locationData) => {
     const states = {
         'Australian Capital Territory': 'ACT',
         'New South Wales': 'NSW',
@@ -65,20 +63,16 @@ const localiseAustralianResults = (locationData) => {
         'Victoria': 'VIC',
         'Western Australia': 'WA',
     };
-
-    if (locationData.country !== 'Australia') {
-        return locationData.country;
-    }
-    return states[locationData.region];
+    return locationData.country === 'Australia' ? states[locationData.region] : locationData.country;
 };
 
 // Generate the title for the embed response ('cutiebot v1.1.0: Weather...')
-const buildTitle = (resolvedLocation) => {
+const buildTitle = (location) => {
     const locationStub = `${pjson.name} v${pjson.version}: Weather`;
     // If the resolved location is South Brisbane then make the title 'QUT' (hack for default location)
-    return resolvedLocation === 'South Brisbane, QLD'
+    return location === 'South Brisbane, QLD'
         ? `${locationStub} at QUT` // 'cutiebot v1.1.0: Weather at QUT'
-        : `${locationStub} in ${resolvedLocation}`; // 'cutiebot v1.1.0: Weather in Place, Country'
+        : `${locationStub} in ${location}`; // 'cutiebot v1.1.0: Weather in Place, Country'
 };
 
 export default {
@@ -97,17 +91,18 @@ export default {
             // Get the location provided by user or, if no option is supplied, set the location to undefined.
             // This is necessary due to discord.js returning null if no option is provided, which will not
             // cause the default parameters in getWeatherData() to be used.
-            const location = await interaction.options.getString('location') ?? undefined;
-            const { resolvedLocation, temperature, description, image } = await getWeatherData(location);
+            const locationOption = await interaction.options.getString('location');
+            const { location, temperature, description, image } = await getWeatherData(locationOption);
 
             weatherEmbed.setImage(image)
-                .setTitle(buildTitle(resolvedLocation))
+                .setTitle(buildTitle(location))
                 .setDescription(`The current temperature is ${temperature}°C.\n\n${description}.`);
             await interaction.followUp({ embeds: [ weatherEmbed ] });
         }
         catch (err) {
             console.log(err);
             // Hack for letting the user know if they've submitted an invalid location :skull:
+            // Currently doesn't do much because the API no longer responds with 200 for an invalid location
             if (err.message.startsWith('WEATHER:')) {
                 await interaction.followUp({ content: `${err.message}`, ephemeral: true });
             } else {
