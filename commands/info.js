195070e349260f0d557252a7029e29c2d0eb74c0
Mistyttm <emmey.leo@gmail.com>
2023-07-14 17:24:50 +1000

    Add changelog button

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,7 @@ import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 const description = `This bot was designed and developed by *Emmey Leo* and *Lillian King* for the IT Crew Discord server. It provides a system to verify that new members are QUT students.
 This project is completely open source and any and all people are allowed to contribute to the repo:
 
-**https://github.com/Mistyttm/cutiebot**`;
+**https://github.com/Mistyttm/cutiebot**\n\n[Changelog](https://github.com/Mistyttm/cutiebot/blob/main/CHANGELOG.md#v120)`;
 
 export default {
     data: new SlashCommandBuilder()
