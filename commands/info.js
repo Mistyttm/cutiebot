7e333da8771f3a6b6a7fd7df86a6d421c116690c
Mistyttm <emmey.leo@gmail.com>
2023-07-14 17:18:39 +1000

    Update changelog

--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,5 @@
-import { pjson } from '../helpers/data.js';
-import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { pjson } from "../helpers/data.js";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 
 const description = `This bot was designed and developed by *Emmey Leo* and *Lillian King* for the IT Crew Discord server. It provides a system to verify that new members are QUT students.
 This project is completely open source and any and all people are allowed to contribute to the repo:
@@ -8,23 +8,27 @@ This project is completely open source and any and all people are allowed to con
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('info')
-        .setDescription('Info about CutieBot'),
+        .setName("info")
+        .setDescription("Info about CutieBot"),
     async execute(interaction) {
         try {
             const infoEmbed = new EmbedBuilder()
-                .setColor(0x00008B)
+                .setColor(0x00008b)
                 .setTitle(`${pjson.name} v${pjson.version}`)
                 .setDescription(description)
                 .setAuthor({
-                    name: 'CutieBot',
-                    iconURL: 'https://cdn.discordapp.com/avatars/1055472048899641365/4d6ff8bb2f760373dd8a41e77300e73a.webp?size=32',
+                    name: "CutieBot",
+                    iconURL:
+                        "https://cdn.discordapp.com/avatars/1055472048899641365/4d6ff8bb2f760373dd8a41e77300e73a.webp?size=32"
                 });
 
-            await interaction.reply({ embeds: [ infoEmbed ] });
+            await interaction.reply({ embeds: [infoEmbed] });
         } catch (err) {
             console.log(err);
-            interaction.reply({ content: 'An error occurred, sorry!', ephemeral: true });
+            interaction.reply({
+                content: "An error occurred, sorry!",
+                ephemeral: true
+            });
         }
-    },
+    }
 };
